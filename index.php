66511a56d9cb18bf5679e3fb5cae689565fbb3fd
AchmadAlfi <achmadalfi1997@gmail.com>
2019-11-01 21:21:37 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis ea modi ratione consequatur doloribus hic officia, quam illum quos vel nesciunt. Ab laborum, quae libero eveniet explicabo ut minima neque?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
